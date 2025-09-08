7171057d3335f95d505d0c5974a48c89e890e6dd
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-08 20:55:24 -0300

    Ejercicio Seis

---
+++ ejercicioSeis.js
@@ -0,0 +1,13 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+
+//*************************Ejercicio 6:
+
+// La tabla de multiplicar
+// Consigna: Crea un programa que le pida al usuario un número y luego imprima su tabla de multiplicar desde el 1 hasta el 10. Usa un ciclo for.
+
+// let numero = parseInt (prompt ("ingrese un numero porfavor"))
+
+// for ( i = 1 ; i <= 10; i++) {
+//     console.log (`${numero} x ${i} = (${numero * i})`);
+// }
