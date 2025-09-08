f89563f5aff877f33039ac67efc0a22a5fa74d2e
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-08 20:41:04 -0300

    Ejercicio Uno

---
+++ ejercicioUno.js
@@ -0,0 +1,19 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+//**********************EJERCICIO UNO 
+
+// ¿Positivo, negativo o cero?
+// Consigna:
+// Vamos a trabajar con números y aprender a clasificarlos. Escribe un programa que le pida al usuario un número cualquiera. El programa deberá analizar si el número ingresado es positivo, negativo o cero. Utiliza estructuras if para resolverlo y muestra un mensaje explicativo en cada caso.
+
+// SOLUCION 
+
+// let numeroIngresado = prompt ("ingrese un numero porfavor");
+
+// if (numeroIngresado > 0) {
+//     alert ("NUMERO POSITIVO");
+// } else if ( numeroIngresado < 0) {
+//     alert ("NUMERO NEGATIVO")
+// } else {
+//     alert ("su numero es 0")
+// }
