2105fbfc7befe5d460348e035e3b17ba7b9b8eae
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-08 20:45:40 -0300

    ejercicio Dos

---
+++ ejercicioDos.js
@@ -0,0 +1,30 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+//*************EJERCICIO DOS 
+
+//  Semáforo inteligente
+// Consigna: Vamos a programar un semáforo. Escribe un programa que le pida al usuario ingresar un color del semáforo: rojo, amarillo o verde. Dependiendo del color ingresado, el programa debe imprimir un mensaje con la acción correspondiente:
+// •
+// Rojo: "Alto, no puedes avanzar."
+// •
+// Amarillo: "Precaución, prepárate para avanzar."
+// •
+// Verde: "Avanza con seguridad." Si el usuario ingresa un color inválido, el programa debe mostrar un mensaje: "Color no reconocido, ingresa rojo, amarillo o verde." Utiliza un switch para resolver este ejercicio.
+
+//SOLUCION 
+
+// let colores = prompt ("Escriba un color : rojo , verde o amarillo, en mayusculas porfavor");
+
+// switch (colores){
+//     case "ROJO":
+//     alert ("Alto, no puedes avanzar")
+//     break;
+//     case "VERDE":
+//     alert ("Avanza con seguridad")
+//     break;
+//     case "AMARILLO":
+//     alert ("Precaución, prepárate para avanzar")
+//     break;
+//     default :
+//     alert ("ERROR. Porfavor elejir rojo , amarillo o verde");
+// }
