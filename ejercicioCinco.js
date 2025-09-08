d04388e08b4d697c19741ef8c7f166f941e6a4b4
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-08 20:52:42 -0300

    Ejercicio Cinco

---
+++ ejercicioCinco.js
@@ -0,0 +1,24 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+
+//***********EJERCICIO CINCO 
+
+//  Encuentra los números pares entre dos valores
+// Consigna: Escribe un programa que le pida al usuario dos números enteros (inicio y fin). El programa debe mostrar todos los números pares que se encuentran entre esos dos valores, incluyendo los límites si son pares. Utiliza un ciclo for para recorrer los números entre el inicio y el fin. Si el número inicial es mayor que el final, el programa debe mostrar un mensaje indicando que los valores son inválidos.
+
+// SOLUCION:
+
+// let numeroInicio = parseInt (prompt ("Ingrese el primer numero porfavor"));
+// let numeroFinal =  parseInt (prompt ("ingrese el segundo numero porfavor"));
+
+
+// if (numeroInicio > numeroFinal){
+//     console.log ("Los Valores son Invalidos");
+// } else{
+//     console.log (`Los numero pares entre ${numeroInicio} y ${numeroFinal} son:`)
+//     for ( let i = numeroInicio ; i <= numeroFinal ; i++ ){
+//         if ( i % 2 === 0){
+//             console.log(i)
+//         }
+//     }
+// }
