c0147ca2bcde01433b1a03352296fa8e942c2fcc
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-08 20:59:11 -0300

    Ejercicio Ocho

---
+++ ejercicioOcho.js
@@ -0,0 +1,19 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+
+//*******************************Ejercicio 8:
+//Encuentra los múltiplos de un número
+// Consigna: Escribe un programa que le pida al usuario un número entero positivo y muestre todos los múltiplos de ese número entre 1 y 100. Utiliza un ciclo for para resolverlo.
+
+// let numero = parseInt (prompt ("ingrese un numero positivo porfavor"));
+
+// if ( isNaN(numero) || numero < 0 ) {
+//     console.log (`Valores invalidos. Porfavor ingresa un numero Positivo`)
+// } else{
+//     console.log (`Los multiplos de ${numero} son`);
+//     for (i = 1 ; i <= 100 ; i++) {
+//         if ( i % numero === 0) {
+//             console.log (i)
+//         }
+//     }
+// }
