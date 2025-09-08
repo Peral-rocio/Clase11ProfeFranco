e8f1e9a2c21ae5e17cdb619e4d98ef8b8520fe1c
Peralta Rocio <peralta.rocio.2229@gmail.com>
2025-09-08 20:48:41 -0300

    Ejercicio Tres

---
+++ ejercicioTres.js
@@ -0,0 +1,18 @@
+//const prompt = require("prompt-sync")({ sigint: true });
+
+//**************EJERCICIO TRES 
+
+//  ¿Sabes contar?
+// Consigna: Crea un programa que le pida al usuario un número positivo. El programa deberá contar desde el 1 hasta ese número e imprimir cada valor en la consola. Usa un ciclo for para realizar la tarea.
+
+//SOLUCION 
+
+// let numeroSolicitado = parseInt (prompt ("ingrese un numero del 0 al 10"));
+
+// if (isNaN(numeroSolicitado) || numeroSolicitado <= 0){
+//     console.log ("Porfavor ingresa un numero positivo")
+// } else{
+//    for( numeroSolicitado = 1 ; numeroSolicitado <= 10 ; numeroSolicitado ++){
+//     console.log (numeroSolicitado);
+// };
+// }
